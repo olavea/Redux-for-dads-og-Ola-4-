8796461e97015ff9dd0f0d792422fb3fb08f8e76
Ola Vea <ola@olavea.com>
2018-01-18 12:34:51 +0100

    Create posts.js

---
+++ Client/data/posts.js
@@ -0,0 +1,46 @@
+const posts = [
+    {
+        "code":"BAcyDyQwcXX",
+        "id":"1161022966406956503",
+        "display_src":"https://scontent-arn2-1.cdninstagram.com/vp/5e53d7473cd92d8162caab6bab1a4b79/5AF078AA/t51.2885-15/e35/26868214_1348431905268961_5798904889887162368_n.jpg"      
+    },
+    {
+        "code":"BAcJeJrQca9",
+        "id":"1160844458347054781",
+        "display_src":"https://scontent-arn2-1.cdninstagram.com/vp/c1cdf72b7d5736221b0f3c0ad5501dcc/5AEE887E/t51.2885-15/s640x640/sh0.08/e35/26072738_1778607539102181_4054382761589342208_n.jpg"
+    },
+    {
+        "code":"BAF_KY4wcRY",
+        "id":"1154606670337393752",
+        "display_src":"https://scontent-arn2-1.cdninstagram.com/vp/8a468e162cf84f1b93d1eb142fc01b46/5ADFE7B9/t51.2885-15/s640x640/sh0.08/e35/26156894_1956221098039360_3770277207604199424_n.jpg"
+    },
+    {
+        "code":"BAPIPRjQce9",
+        "id":"1157179863266871229",
+        "display_src":"https://scontent-arn2-1.cdninstagram.com/vp/2ce8ad84ba055da29d1e9869f1e5cefb/5AE1F13D/t51.2885-15/s640x640/sh0.08/e35/26070727_187378232004933_2783718950515507200_n.jpg"
+    },
+    {
+        "code":"-hZh6IQcfN",
+        "id":"1126293663140399053",
+        "display_src":"https://scontent-arn2-1.cdninstagram.com/vp/38eafd7d5843610e91c2b16eb7baa4fe/5AE75871/t51.2885-15/s640x640/sh0.08/e35/26295729_156490118325358_1763657345767833600_n.jpg"
+    },
+    {
+        "code":"BAhvZrRwcfu",
+        "id":"1162418651480049646",
+        "display_src":"https://twitter.com/batcatsmirre/status/953640776256368640"
+    },
+    {
+       "code":"BAAJqbOQcW5",
+       "id":"1152964002473690553",
+       "display_src":"https://pbs.twimg.com/media/DTwDOVTWAAAKfoK.jpg"
+    },
+    {
+       "code":"_4jHytwcUA",
+       "id":"1150824171912152320",
+       "display_src":"https://pbs.twimg.com/media/DNP8f4TXUAA9wNF.jpg"
+    }
+ ];
+ 
+ 
+ export default posts;
+ 
